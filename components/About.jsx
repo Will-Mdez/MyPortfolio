59fd0eb059ec7e32e5197e946eb928fd560ed43b
William Mendez <82000613+Will-Mdez@users.noreply.github.com>
2023-09-07 12:10:00 -0600

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -10,11 +10,12 @@ export const About = () => {
                 <p className='uppercase text-xl tracking-widest text-[#5651e5] '>About</p>
                 <h2 className='py-4'>Who I am</h2>
                 <p className='py-2 text-gray-600' >I am not your normal developer</p>
-                <p className='py-2 text-gray-600'> I am Will Mendez Computer Engineering student, with great interest in Artificial Intelligence and 
-                    Software development. With the ability to learn and apply my knowledge to the professional field.
-                     Looking for an opportunity to develop my skills and gain experience.
+                <p className='py-2 text-gray-600'> I am a Computer Engineer, passionate about artificial intelligence and software development. 
+                  I have demonstrated my ability to design and develop efficient web and mobile applications, reducing processing times and improving data management.
+                  I have also participated in the team development of a Rappi-type sales software, contributing my skills in the design of the architecture and the implementation of key functionalities. 
+                  I am always looking for new challenging and collaborative projects where I can apply my knowledge.
                 </p>
-                <p className='py-2 text-gray-600'> I have been speed the last 4 years Studing Computindg Engineer, 
+                <p className='py-2 text-gray-600'> I have been speed the last 5 years Studing Computindg Engineer, 
                     and be building some projects in the school, like 
                     User Interface, API&apos;s, Apps in Android Studio, some little projects 
                     from IA, and something else.
